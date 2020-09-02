7858b1e90d63ba49e6e85bd9e4c15e7a06c876a3
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-09-02 15:34:22 +0900

    Add cursor style to buttons + cleaned up unused code

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import useDebounce from '../hooks/use-debounce';
 import styles from './SearchForm.module.css';
 
-import SearchResultsList from './SearchResultsList';
-
 const fetcher = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
@@ -34,17 +32,15 @@ export default function SearchForm(props) {
   }, [debouncedQuery]);
 
   return (
-    <>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.horizontal}>
-          <svg className={styles.icon} aria-hidden="true" focusable="false" role="presentation" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-            <title>Search icon</title>
-            <path d="M19.5 21.5L13.6155 15.1628" stroke="currentColor" strokeWidth="1.75" />
-            <circle cx="9.5" cy="9.5" r="7" stroke="currentColor" strokeWidth="1.75" />
-          </svg>
-          <input className={styles.input} type='text' value={query} onChange={handleChange} placeholder='Search by movie title' />
-        </div>
-      </form>
-    </>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.horizontal}>
+        <svg className={styles.icon} aria-hidden="true" focusable="false" role="presentation" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+          <title>Search icon</title>
+          <path d="M19.5 21.5L13.6155 15.1628" stroke="currentColor" strokeWidth="1.75" />
+          <circle cx="9.5" cy="9.5" r="7" stroke="currentColor" strokeWidth="1.75" />
+        </svg>
+        <input className={styles.input} type='text' value={query} onChange={handleChange} placeholder='Search by movie title' />
+      </div>
+    </form>
   );
 }
